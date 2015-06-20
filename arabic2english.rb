ca8cabfc665bd45a6b1be7d8d99fd0e51e99de97
Yunus Eren Guzel <exculuber@gmail.com>
2015-06-20 14:34:08 +0300

    added developers note

--- arabic2english.rb
+++ arabic2english.rb
@@ -1,3 +1,13 @@
+#
+# author: Yunus Eren Guzel
+# email: yunuserenguzel@gmail.com
+# date: 20.06.2015
+#
+# Notes: the number conversion is implemented
+# based on Mac's say command in the terminal
+#
+#
+
 require './convert_hundreds'
 require './convert_thousands'
 require './convert_tens'
