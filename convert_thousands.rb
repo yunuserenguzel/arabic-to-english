e8dbfad0ec09a915e76db78d381bd30bcee94351
Yunus Eren Guzel <exculuber@gmail.com>
2015-06-20 13:48:21 +0300

    convert thousands funciton is defined

--- convert_thousands.rb
+++ convert_thousands.rb
@@ -1,4 +1,7 @@
 def convert_thousands(value)
   left_hand = value/1000
-  convert
+  right_hand = convert_number(value%1000)
+  right_hand = " #{right_hand}" if right_hand
+  thousands = left_hand == 1 ? ' thousand' : ' thousands'
+  "#{convert_number(left_hand)}#{thousands}#{right_hand}"
 end
