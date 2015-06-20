5ec17f4f908ef69edcd7d1eba361b67404bf91cf
Yunus Eren Guzel <exculuber@gmail.com>
2015-06-20 13:34:48 +0300

    refactor for convert_two_steps to convert_tens

--- arabic2english.rb
+++ arabic2english.rb
@@ -1,6 +1,6 @@
 require './convert_hundreds'
 require './convert_thousands'
-require './convert_two_steps'
+require './convert_tens'
 require './convert_digit'
 require './convert_number'
 number = ARGV[0].to_i
