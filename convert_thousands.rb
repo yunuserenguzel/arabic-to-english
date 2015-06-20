7e250f95a5e59360b6c72170b71a0fa8678120a0
Yunus Eren Guzel <exculuber@gmail.com>
2015-06-20 23:42:54 +0300

    convert thousands extra tests are added and revealed bugs are fixed

--- convert_thousands.rb
+++ convert_thousands.rb
@@ -1,6 +1,8 @@
 def convert_thousands(value)
-  left_hand = value/1000
-  right_hand = convert_number(value%1000)
-  right_hand = " #{right_hand}" if right_hand
-  "#{convert_number(left_hand)} thousand#{right_hand}"
+  if value > 999 && value < 1000000
+    left_hand = value/1000
+    right_hand = convert_number(value%1000)
+    right_hand = " #{right_hand}" if right_hand
+    "#{convert_number(left_hand)} thousand#{right_hand}"
+  end
 end
--- tests/convert_thousands_spec.rb
+++ tests/convert_thousands_spec.rb
@@ -2,7 +2,22 @@ require './tests/rspec_helper'
 
 describe 'convert_thousands' do
 
-  it 'should should convert 1000 to one thousand' do
+  it 'should convert 1000 to one thousand' do
     expect(convert_thousands(1000)).to eq('one thousand')
+  end
+
+  it 'should convert 4001 to four thousand one' do
+    expect(convert_thousands(4001)).to eq('four thousand one')
+  end
+
+  it 'should convert 25023 to twenty five thousand twenty three' do
+    expect(convert_thousands(25023)).to eq('twenty five thousand twenty three')
+  end
+
+  it 'should convert 107376 to seven thousand three hundred seventy six' do
+    expect(convert_thousands(107376)).to eq('one hundred seven thousand three hundred seventy six')
+  end
+  it 'should return nil for 134' do
+    expect(convert_thousands(134)).to eq(nil)
   end
 end
