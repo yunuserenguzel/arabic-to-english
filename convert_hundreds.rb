d8cf01a4f69a005ead00a41211b32569e32b192f
Yunus Eren Guzel <exculuber@gmail.com>
2015-06-20 13:47:29 +0300

    returning string format is changed

--- convert_hundreds.rb
+++ convert_hundreds.rb
@@ -1,7 +1,7 @@
 def convert_hundreds(value)
   left_digit = (value/100)
-  hundreds = left_digit == 1 ? 'hundred' : 'hundreds'
+  hundreds = left_digit == 1 ? ' hundred' : ' hundreds'
   right_hand = convert_number(value%100)
-  right_hand = "and #{right_hand}" if right_hand
-  "#{convert_digit(left_digit)} #{hundreds} #{right_hand}"
+  right_hand = " #{right_hand}" if right_hand
+  "#{convert_digit(left_digit)}#{hundreds}#{right_hand}"
 end
