518286a91dc40387e45f8b164205bd7c07103536
Yunus Eren Guzel <exculuber@gmail.com>
2015-06-20 14:24:19 +0300

    bug fix for thousands

--- convert_number.rb
+++ convert_number.rb
@@ -1,11 +1,11 @@
-def convert_number(value)
+def convert_number(value = value.to_i)
   if value < 10
     convert_digit(value)
   elsif value < 100
     convert_tens(value)
   elsif value < 1000
     convert_hundreds(value)
-  elsif value < 10000
+  elsif value < 1000000
     convert_thousands(value)
   end
 end
