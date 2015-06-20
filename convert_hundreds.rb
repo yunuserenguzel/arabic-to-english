6627f756e648c1103218c976588c59ecdaff3e41
Yunus Eren Guzel <exculuber@gmail.com>
2015-06-20 13:38:36 +0300

    convert hundreds digit problem in two steps is fixed

--- convert_hundreds.rb
+++ convert_hundreds.rb
@@ -1,7 +1,7 @@
 def convert_hundreds(value)
   left_digit = (value/100)
   hundreds = left_digit == 1 ? 'hundred' : 'hundreds'
-  two_steps = convert_tens(value%100)
-  two_steps = "and #{two_steps}" if two_steps
-  "#{convert_digit(left_digit)} #{hundreds} #{two_steps}"
+  right_hand = convert_number(value%100)
+  right_hand = "and #{right_hand}" if right_hand
+  "#{convert_digit(left_digit)} #{hundreds} #{right_hand}"
 end
