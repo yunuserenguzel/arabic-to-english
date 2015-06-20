bcf18a2703943d57493939cb68e889d5bb448ed5
Yunus Eren Guzel <exculuber@gmail.com>
2015-06-20 14:28:58 +0300

    plural hundred and thousand are removed

--- convert_hundreds.rb
+++ convert_hundreds.rb
@@ -1,7 +1,6 @@
 def convert_hundreds(value)
   left_digit = (value/100)
-  hundreds = left_digit == 1 ? ' hundred' : ' hundreds'
   right_hand = convert_number(value%100)
   right_hand = " #{right_hand}" if right_hand
-  "#{convert_digit(left_digit)}#{hundreds}#{right_hand}"
+  "#{convert_digit(left_digit)} hundreds#{right_hand}"
 end
--- convert_thousands.rb
+++ convert_thousands.rb
@@ -2,6 +2,5 @@ def convert_thousands(value)
   left_hand = value/1000
   right_hand = convert_number(value%1000)
   right_hand = " #{right_hand}" if right_hand
-  thousands = left_hand == 1 ? ' thousand' : ' thousands'
-  "#{convert_number(left_hand)}#{thousands}#{right_hand}"
+  "#{convert_number(left_hand)} thousand#{right_hand}"
 end
