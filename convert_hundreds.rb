8ecaa783c6ca488f2f930bee3608cb5271047dbd
Yunus Eren Guzel <exculuber@gmail.com>
2015-06-20 14:29:31 +0300

    string fix

--- convert_hundreds.rb
+++ convert_hundreds.rb
@@ -2,5 +2,5 @@ def convert_hundreds(value)
   left_digit = (value/100)
   right_hand = convert_number(value%100)
   right_hand = " #{right_hand}" if right_hand
-  "#{convert_digit(left_digit)} hundreds#{right_hand}"
+  "#{convert_digit(left_digit)} hundred#{right_hand}"
 end
